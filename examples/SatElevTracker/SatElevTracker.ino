c22a4af2d3fedf872109822e9dd21990cf069fbd
Don Wade <don_wade@rogers.com>
2026-05-10 16:15:33 -0400

    adapt for m5 core2

--- examples/SatElevTracker/SatElevTracker.ino
+++ examples/SatElevTracker/SatElevTracker.ino
@@ -1,5 +1,8 @@
 #include <TinyGPSPlus.h>
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
+#include <M5Unified.h>
+#include "MultipleSatellite.h"
+
 /*
    This sample code tracks satellite elevations using TinyGPSCustom objects.
 
@@ -9,16 +12,25 @@
    It requires the use of SoftwareSerial and assumes that you have a
    4800-baud serial GPS device hooked up on pins 4(RX) and 3(TX).
 */
-static const int RXPin = 4, TXPin = 3;
-static const uint32_t GPSBaud = 4800;
+static const int RXPin = 13, TXPin = 27;
+static const uint32_t GPSBaud = 115200;
+
+//static const int RXPin = 4, TXPin = 3;
+//static const uint32_t GPSBaud = 4800;
 static const int MAX_SATELLITES = 40;
 static const int PAGE_LENGTH = 40;
 
+#if 0
 // The TinyGPSPlus object
-TinyGPSPlus gps;
+    TinyGPSPlus gps;
+    // The serial connection to the GPS device
+    SoftwareSerial ss(RXPin, TXPin);
+#else
+    // Create an instance of MultipleSatellite, assuming we use the Serial1 seri
+    MultipleSatellite gps(Serial1, GPSBaud, SERIAL_8N1, RXPin, TXPin);
+#endif
 
-// The serial connection to the GPS device
-SoftwareSerial ss(RXPin, TXPin);
+
 
 TinyGPSCustom totalGPGSVMessages(gps, "GPGSV", 1); // $GPGSV sentence, first element
 TinyGPSCustom messageNumber(gps, "GPGSV", 2);      // $GPGSV sentence, second element
@@ -35,15 +47,38 @@ struct
 
 void setup()
 {
+
+  m5::M5Unified::config_t cfg = M5.config();
+    //std::cout << "Type of cfg: " << typeid(cfg).name() << std::endl;
+
+    // Set the items you want to configure. Omit the following two lines if
+    cfg.serial_baudrate = 115200;
+    cfg.output_power = true;
+    cfg.output_power = true;
+
+    M5.begin(cfg);
+
+    M5.Power.setExtOutput(false);  // reset gps
+    delay(1000);
+    M5.Power.setExtOutput(true);    // restart gps
+
   Serial.begin(115200);
-  ss.begin(GPSBaud);
+  gps.begin();
 
   Serial.println(F("SatElevTracker.ino"));
   Serial.println(F("Displays GPS satellite elevations as they change"));
   Serial.print(F("Testing TinyGPSPlus library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
   Serial.println(F("by Mikal Hart"));
   Serial.println();
-  
+
+  String version = gps.getGNSSVersion();
+  Serial.printf("GNSS SW=%s\r\n", version.c_str());
+  delay(1000);
+  // Set satellite mode
+  gps.setSatelliteMode(SATELLITE_MODE_GPS);
+
+  gps.setSystemBootMode(BOOT_FACTORY_START);
+ 
   // Initialize all the uninitialized TinyGPSCustom objects
   for (int i=0; i<4; ++i)
   {
@@ -55,9 +90,39 @@ void setup()
 void loop()
 {
   // Dispatch incoming characters
-  if (ss.available() > 0)
+  // if (gps.available() > 0)
+  gps.updateGPS();
+
+  static uint16_t satCount;
+  uint16_t satCountNow = gps.satellites.value();
+
+  if (satCount != satCountNow)
   {
-    gps.encode(ss.read());
+    Serial.printf("sat count = %d\n", satCountNow);
+    satCount = satCountNow;
+  }
+
+  static double keepLat, keepLng;
+  static uint32_t loopCtr = 0;
+
+  double nowLat, nowLng;
+  loopCtr++;
+
+  if (gps.location.isUpdated()) {
+      nowLat = gps.location.lat();
+      nowLng = gps.location.lng();
+
+      if (nowLat != keepLat || nowLng != keepLng)
+      {
+          Serial.printf("%8d %10.8f %10.8f \n", loopCtr, nowLat, nowLng);
+      }
+  }
+
+  delay(1000);
+
+  gps.updateGPS();
+  {
+    gps.encode(gps.read());
    
     if (totalGPGSVMessages.isUpdated())
     {
@@ -112,7 +177,7 @@ void IntPrint(int n, int len)
 
 void TimePrint()
 {
-  if (gps.time.isValid())
+  if (gps.time.isUpdated())
   {
     if (gps.time.hour() < 10)
       Serial.print(F("0"));
